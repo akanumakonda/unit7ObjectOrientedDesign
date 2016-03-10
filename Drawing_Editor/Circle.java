b0eec8eca825a5d3f300138b5889f841b77ed4ff
akanumakonda <aanumakonda@gmail.com>
2016-03-09 19:52:19 -0600

    Update Circle.java

--- Drawing_Editor/Circle.java
+++ Drawing_Editor/Circle.java
@@ -1,4 +1,9 @@
-
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 /**
  * Write a description of class Circle here.
@@ -12,41 +17,45 @@ public class Circle extends Shape
     private Ellipse2D.Double newCircle;
     private double width;
     private double height;
+    private double x;
+    private double y;
     /**
      * Default constructor for objects of class Circle
      */
-    public Circle()
+    public Circle(Point2D.Double center, double radius, Color color)
     {
         // initialise instance variables
-        width = 2*super.getRadus();
+        super(center, radius, color);
+        x = center.getX();
+        y  = center.getY();
+        width = 2*super.getRadius();
         height = width;
     }
 
     public boolean isInside(Point2D.Double point)
-    if (newCircle.contains(point) == true)
     {
-        return true;
+        if (newCircle.contains(point.getX(), point.getY()))
+        {
+            return true;
+        }
+        return false;
     }
-    return false;
-    }
-    public void draw(Graphics2D g2, boolean filled)
+
+    public  void draw(Graphics2D g2, boolean filled)
     {
         newCircle = new Ellipse2D.Double(x, y , width , height);
         if (filled == true)
         {
-            g2.setPaint(shapeColor);
-            g2.fill(newCircle)
+            g2.setPaint(color);
+            g2.fill(newCircle);
         }
         else
         {
             g2.setPaint(Color.WHITE);
             g2.fill(newCircle);
-            g2.setPaint(shapeColor);
-            g2.draw(newCircle)
-            
+            g2.setPaint(color);
+            g2.draw(newCircle);
         }
-        
-        
-    }
 
+    }
 }
